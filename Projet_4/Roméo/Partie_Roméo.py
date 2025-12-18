db05fdfdef0df2ea86a50f0cb2236fabf254b449
lu6an <lucian.theaux@icloud.com>
2025-12-18 16:13:48 +0100

    Ajout de Projet 4

---
+++ Projet_4/Roméo/Partie_Roméo.py
@@ -0,0 +1,19 @@
+"""Ici on importe les fichiers nécéssaires pour le projet"""
+from turtle import*
+from nsi_ui import*
+
+
+title("Projet_4 Scrabble")
+setup(1.0, 1.0)
+
+
+
+def clear():
+    clearscreen()
+button("clear",clear)
+
+
+
+
+listen()
+mainloop()
