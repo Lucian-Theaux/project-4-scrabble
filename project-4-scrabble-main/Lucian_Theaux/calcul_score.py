9b2dc279670d0a5e8c97fe1929e79fa8aa3f5217
promised-neverland <rieversong@gmail.com>
2026-01-08 19:48:36 +0100

    Add files via upload

---
+++ project-4-scrabble-main/Lucian_Theaux/calcul_score.py
@@ -0,0 +1,73 @@
+import turtle as tt
+
+def ajout_score(score:int,mot:str):
+    """
+    ajout_score: Écrit dans le fichier scores.txt le score donné en entrée
+    - score: Prend en entrée un integer
+    """
+    with open('scores.txt', 'a', encoding='utf-8') as F:                    # Ouvre le fichier en mode ajout
+        F.write(f'{score};{mot}\n')                                         # Écrit le score et le mot dans le fichier
+
+def scoreboard(length:int):
+    """
+    scoreboard: Récupère tous les scores du fichier texte scores.txt pour créer un scoreboard length score du meilleur au plus petit
+    - length: Prend en entrée un integer
+    """
+    scoreboard_dict = {}                                                        # Dictionnaire vide pour stocker les scores
+    with open('scores.txt', 'r', encoding='utf-8') as F:                        # Ouvre le fichier en mode lecture
+        scoreboard_list = F.read().split('\n')                                   # Lit le fichier et sépare les lignes en une liste
+    print(scoreboard_list)                                                      # Liste des scores lus dans le fichier       
+    for elem in scoreboard_list:                                             # Parcourt chaque élément de la liste 
+        if elem == '':                                                      # Si l'élément est une chaîne vide, on:
+            continue
+        value,key = elem.split(';')                                         # Sépare l'élément en score (value) et mot (key)
+        scoreboard_dict[key] = int(value)                                   # Ajoute le score et le mot au dictionnaire
+    scoreboard_dict = dict(sorted(scoreboard_dict.items(), key= lambda item: item[1]))  # Trie le dictionnaire par score croissant
+    print(scoreboard_dict)
+    tt.fillcolor('white')
+    tt.penup()
+    tt.goto(300, -300)                                                      # Position du tableau
+    tt.begin_fill()                                                         # Fond du tableau
+    tt.setheading(0)                                                        # Orientation de la tortue      
+    tt.fd(100)                                                              # Largeur du tableau
+    tt.rt(90)                                                               # Hauteur du tableau    
+    tt.fd(50)
+    tt.rt(90)
+    tt.fd(100)
+    tt.rt(90)
+    tt.fd(50)
+    tt.end_fill()
+    tt.rt(90)
+    tt.fd(95)
+    tt.rt(90)
+    tt.write('CLASSEMENT', font=('Minecraft',30, 'bold'))                   # Titre du tableau
+    tt.fd(30)                                                               # Espace entre le titre et les scores 
+    i = 0
+    if len(list(scoreboard_dict)) < length:                                 # Si le nombre de scores est inférieur à length, on affiche tous les scores disponibles
+        for i in range(1,len(list(scoreboard_dict))+1):                     # On affiche tous les scores disponibles
+            key = list(scoreboard_dict)[-i]                                 # Récupère la clé (mot) du i-ème score le plus élevé
+            value = list(scoreboard_dict.values())[-i]                      # Récupère la valeur (score) du i-ème score le plus élevé
+            if i == 1:                                                      # Si c'est le meilleur score:
+                tt.color("#D1A90F")                                                  # Couleur spéciale pour le meilleur score
+                tt.write(f'{value} ☞ {key}', font=('Minecraft',20, 'italic'))          # Affiche le score et le mot
+                tt.color('black')
+                tt.fd(20)
+            else:                                                           # Pour les autres scores:
+                tt.write(f'{value} ☞ {key}', font=('Minecraft',20, 'italic'))          # Affiche le score et le mot en noir simple
+                tt.fd(20)
+            i+= 1
+    else:
+
+        for i in range(1,length+1):                                         # Sinon, on affiche seulement les length des meilleurs scores                                    
+            key = list(scoreboard_dict)[-i]                                 # Récupère la clé (mot) du i-ème score le plus élevé
+            value = list(scoreboard_dict.values())[-i]                      # Récupère la valeur (score) du i-ème score le plus élevé
+            if i == 1:                                                      # Si c'est le meilleur score:
+                tt.color("#D1A90F")                                         # Couleur spéciale pour le meilleur score   
+                tt.write(f'{value} ☞ {key}', font=('Minecraft',20, 'italic'))        # Affiche le score et le mot
+                tt.color('black')
+                tt.fd(20)   
+            else:                                                               # Pour les autres scores:
+                tt.write(f'{value} ☞ {key}', font=('Minecraft',20, 'italic'))      # Affiche le score et le mot en noir simple
+                tt.fd(20)
+            i+= 1
+        
---
+++ project-4-scrabble-main/scrabble.py
@@ -0,0 +1,49 @@
+# coding=utf-8
+#* Projet réalisé par :
+#* Roméo HUYNH
+"""Ici on importe les fichiers nécéssaires pour le projet"""
+import turtle as tt
+import tkinter as tk
+from RIVES_Raphaelle.search import recherche
+from Lucian_Theaux.tuiles import tuile, letter_impression
+from Lucian_Theaux.calcul_score import ajout_score, scoreboard
+from RIVES_Raphaelle.dictionnaire_lettres import points
+from RIVES_Raphaelle.nombre_de_points import Score
+from Romeo.bareme_points import bareme_scrabble
+import time
+
+#* -------------- Zone commentaire --------------------
+
+"""Ici on initialise quelques paramètres utiles"""
+tt.title("Projet_4 Scrabble")                     # Titre de la fenêtre Turtle
+tt.speed(8)                                       # Régler la vitesse initiale
+tt.hideturtle()                                   # Cacher le curseur de la tortue
+tt.down()                                         # Abaisser le stylo pour commencer à dessiner
+tt.screensize(300,300)                            # Configurer la taille de l'écran
+tt.speed(0)                                       # Régler la vitesse de dessin au maximum
+
+root = tk.Tk()
+
+#──────────────────── Interface graphique ─────────────────
+tt.title('Scrabble.io')                           # Titre de la fenêtre Turtle
+tt.setup(1.0,1.0)                                 # Configurer la taille de la fenêtre Turtle
+root.title('Scrabble.io')                         # Titre de la fenêtre Tkinter
+
+# ────────────────── Zone d’interaction ────────────────────
+right = tk.Frame(root)                            # Créer un frame pour la zone de saisie
+input_label = tk.Label(right,text='Entrer un mot (sans accents):')                  # Label pour indiquer à l'utilisateur d'entrer un mot
+input_label.pack()                                  # Placer le label dans le frame
+lettre = tk.Entry(right)                          # Champ de saisie pour le mot
+lettre.insert(0, 'exemple: tortue')                 # Texte d'exemple
+lettre.pack()      
+slider_metric = tk.Scale(right, orient='horizontal')  # Curseur pour ajuster la taille
+slider_metric.pack()
+slider_metric.set(10)
+send_button = tk.Button(right,text='Envoyer',command=lambda:Score(lettre, points, slider_metric))  # Bouton d'envoi
+send_button.pack()
+show_button = tk.Button(right,text='Afficher le barème', command=bareme_scrabble)  # Bouton du barème
+show_button.pack()
+right.grid(column=1, row=0)                       # Placer le frame sur la grille
+
+tt.listen()                                       # Écouter les événements
+tt.mainloop();root.mainloop()                     # Lancer les deux boucles principales
