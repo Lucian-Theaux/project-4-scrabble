4b21b82a5a7b878f2770484306c6ed99641bfdf6
Romeo Huynh <romeohuynh@hotmail.com>
2026-03-26 22:32:50 +0100

    ajout commentaires

--- Scrabble Trophée NSI/scrabble.py
+++ Scrabble Trophée NSI/scrabble.py
@@ -1,18 +1,18 @@
 
-def creation_plateau():
-    return [['' for i in range(15)] for i in range(15)]
+def creation_plateau():  # Crée un plateau vide 15x15
+    return [['' for i in range(15)] for i in range(15)]  # Liste de listes vides
 
-def ajout_mot_horizontal(x:int,y:int,mot:str,plateau:list):
-    i = 0
-    for lettre in mot:
-        plateau[y][x+i] = lettre.capitalize()
-        i += 1
+def ajout_mot_horizontal(x:int,y:int,mot:str,plateau:list):  # Ajoute un mot horizontalement
+    i = 0  # Index de la lettre
+    for lettre in mot:  # Pour chaque lettre du mot
+        plateau[y][x+i] = lettre.capitalize()  # Placer la lettre en majuscule sur le plateau
+        i += 1  # Passer à la colonne suivante
 
-def ajout_mot_vertical(x:int,y:int,mot:str,plateau:list):
-    i = 0
-    for lettre in mot:
-        plateau[y+i][x] = lettre.capitalize()
-        i += 1
+def ajout_mot_vertical(x:int,y:int,mot:str,plateau:list):  # Ajoute un mot verticalement
+    i = 0  # Index de la lettre
+    for lettre in mot:  # Pour chaque lettre du mot
+        plateau[y+i][x] = lettre.capitalize()  # Placer la lettre en majuscule verticalement
+        i += 1  # Passer à la ligne suivante
 
 # while continuer:
 #     ask = input('Mot [Vide si arrêter] : ')
@@ -34,4 +34,3 @@ def ajout_mot_vertical(x:int,y:int,mot:str,plateau:list):
 
 #     for liste in plateau:
 #         print(liste)
-
